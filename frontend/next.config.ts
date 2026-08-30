e91b8318fcd3d4837b81b4862507de990eb334da
Poorvith Devang <poorvithdevang861@gmail.com>
2026-08-30 16:38:25 +0530

    changes done to ui

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const backendUrl = process.env.BACKEND_URL || "http://localhost:4000";
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
   allowedDevOrigins: ["172.16.185.67", "localhost"],
   async rewrites() {
     return [
